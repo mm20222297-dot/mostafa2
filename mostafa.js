b22ab5e4afc08aa4d7d7d8556b7760be51bad6c8
mm20222297-dot <mm20222297@sva.edu.eg>
2026-01-19 15:14:16 +0200

    Add files via upload

--- mostafa.js
+++ mostafa.js
@@ -1,31 +1,7 @@
-const imeg = document.querySelector(".imeg");
-const hourHand = document.querySelector(".hour");
-const minuteHand = document.querySelector(".minute");
-const secondHand = document.querySelector(".second");
-const modeSwitch = document.querySelector(".mode-switch");
-
-if (localStorage.getItem("mode") === "Dark Mode") {
-  imeg.classList.add("dark");
-  modeSwitch.textContent = "Light Mode";
-}
-
-modeSwitch.addEventListener("click", () => {
- imeg.classList.toggle("dark");
-  const isDarkMode = imeg.classList.contains("dark");
-  modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
-  localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
+const scrollContainer =document .querySelectorAll('.products');
+for(const item of scrollContainer ){
+item.addEventListener('wheel',(evt)=>{
+    evt.preventDefault();
+    item.scrollLeft += evt.deltaY;
 });
-
-const updateTime = () => {
-  let date = new Date();
-  let secToDeg = (date.getSeconds() / 60) * 360;
-  let minToDeg = (date.getMinutes() / 60) * 360;
-  let hrToDeg = (date.getHours() / 12) * 360;
-
-  secondHand.style.transform = `rotate(${secToDeg}deg)`;
-  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
-  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
 };
-
-setInterval(updateTime, 1000);
-updateTime();
